e33eec5360ac2068e6f56a18f0455beacf2d5184
ShinyChang <ublue724@gmail.com>
2015-10-03 11:57:32 +0800

    dev env

---
+++ webpack.config.dev.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'webpack-hot-middleware/client',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/build/'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    externals: {
+        react: 'React',
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel?stage=0'],
+            include: path.join(__dirname, 'src')
+        }]
+    }
+};
